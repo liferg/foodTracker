0aae6f807c78e592208df02f46bf6fc8872930b4
liferg <45667185+liferg@users.noreply.github.com>
2022-01-05 15:42:19 -0600

    Added bootstrap files, and added a search bar for the add food page

--- log/models.py
+++ log/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+# class Food (models.Model):
+#     name = models.TextField()
+#     calories = models.IntegerField()
+#     gramsFat = models.IntegerField()
+#     gramsCarbs = models.IntegerField()
+#     gramsProtein = models.IntegerField()
+
+# class SingleDayFood(models.Model):
+#     date = models.DateField()
+#     breakfastFoods = models.QuerySet
+#     lunchFoods = models.QuerySet
+#     dinnerFoods = models.QuerySet
+#     snacks = models.QuerySet
+
+
