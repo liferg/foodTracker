d3accd3911ee4dbd8d241c0c1923f99bdc5cc266
liferg <45667185+liferg@users.noreply.github.com>
2022-01-08 13:44:19 -0600

    Figured out how to get data from get request to food API using requests package

--- code.py
+++ code.py
@@ -1,10 +1,6 @@
-import http.client
+import requests
 
-def searchFood(foodName):   
-    conn = http.client.HTTPSConnection("api.nal.usda.gov")
-    payload = ''
-    headers = {}
-    conn.request("GET", "/fdc/v1/foods/search?query=apple&pageSize=1&api_key=oSCPMwfMvZkiRn3AwPrcf17WF5wVVqm0eUAxf3Ha", payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
+url = "https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=1&api_key=oSCPMwfMvZkiRn3AwPrcf17WF5wVVqm0eUAxf3Ha"
+response = requests.get(url)
+data = response.json()
+print(data.get('foods')[0].get('foodNutrients'))
--- log/views.py
+++ log/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+import http.client
 
 def logView(request):
     return render(request, 'log.html')
@@ -12,8 +13,9 @@ def foodSelectedView(request):
     return render(request, 'foodSelectedPage.html')
 
 def searchEnteredFood(request):
+    print(data.decode("utf-8"))
     search = request.GET.get('searchTerm')
     response = {
-        'search' : search
+       'search' : search
     }
     return JsonResponse(response)
